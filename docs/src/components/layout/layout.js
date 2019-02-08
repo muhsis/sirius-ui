494cc3d0b72c4471879113814309e6cb3ef7f1e1
Emre Kılınç <emre@nitro.io>
2019-02-08 14:34:40 +0300

    docs: modify docs site layout

---
+++ docs/src/components/layout/layout.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { ThemeProvider } from 'styled-components'
+import { StaticQuery, graphql } from 'gatsby'
+
+import { theme, Normalize } from '../../../../src'
+console.log('Active theme', theme)
+
+class Layout extends Component {
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
+          }
+        `}
+        render={data => (
+          <ThemeProvider theme={theme}>
+            <>
+              <Normalize />
+              <h1>KargoBurada UI!</h1>
+              {this.props.children}
+            </>
+          </ThemeProvider>
+        )}
+      />
+    )
+  }
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,20 +1,33 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { Layout } from '../components/layout'
+import SEO from '../components/seo'
+import { Box, Text, Paragraph, boxiconsRegular } from '../../../src'
+const { CodeBlock } = boxiconsRegular
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Box
+      border="1px solid"
+      borderColor="primary"
+      borderRadius=".5rem"
+      p={3}
+      mx={3}
+    >
+      <Paragraph>
+        <CodeBlock size="30" />
+        Maecenas faucibus mollis interdum. Curabitur blandit tempus porttitor.
+        Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur
+        blandit tempus porttitor. Vestibulum id ligula porta felis euismod
+        semper.
+      </Paragraph>
+      <Text color="primary" fontSize={1}>
+        Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+        lacinia odio sem nec elit. Maecenas sed diam eget risus varius blandit
+        sit amet non magna.
+      </Text>
+    </Box>
   </Layout>
 )
 
