f7cfaa95bad5ce9997dd9c5d272ec050e39af7dd
Emre Kılınç <emre@nitro.io>
2019-02-14 16:24:06 +0300

    docs: add example box documentation templates

---
+++ src/elements/box/docs/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Box from '../src/Box'
+
+// Code examples
+import exampleBoxBasic from './examples/basic.example'
+
+const title = 'Box'
+const subTitle = 'A common component. A building block.'
