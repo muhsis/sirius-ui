f78a585cd213281bdc7812429cecdf91445726fc
Emre Kılınç <emre@nitro.io>
2019-02-18 13:41:51 +0300

    feat(theme): add headings typo theme values

--- src/theming/typography.js
+++ src/theming/typography.js
@@ -20,3 +20,28 @@ export const fontWeightBold = 700
 export const lineHeightBase = 1.5
 export const lineHeightSm = 1.5
 export const lineHeightLg = 1.5
+
+export const headingsMarginBottom = '.5rem'
+export const headingsFontFamily = fontFamily
+export const headingsFontWeight = 500
+export const headingsLineHeight = 1.2
+export const headingsColor = 'inherit'
+
+export const h1FontSize = '2.5rem'
+export const h2FontSize = '2rem'
+export const h3FontSize = '1.75rem'
+export const h4FontSize = '1.5rem'
+export const h5FontSize = '1.25rem'
+export const h6FontSize = '1rem'
+
+export const display1Size = '6rem'
+export const display2Size = '5.5rem'
+export const display3Size = '4.4rem'
+export const display4Size = '3.5rem'
+
+export const display1Weight = 300
+export const display2Weight = 300
+export const display3Weight = 300
+export const display4Weight = 300
+
+export const displayLineHeight = headingsLineHeight
