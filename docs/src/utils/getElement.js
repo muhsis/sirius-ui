c82805e21b2a062b9f2c3eaf4423645b48fe6989
Emre Kılınç <emre@nitro.io>
2019-03-13 14:01:52 +0300

    docs: getElement method

--- docs/src/utils/getElement.js
+++ docs/src/utils/getElement.js
@@ -1,8 +1,28 @@
 // Get
+import avatarDocs from '../../../src/elements/avatar/docs'
+import badgeDocs from '../../../src/elements/badge/docs'
 import boxDocs from '../../../src/elements/box/docs'
+import buttonDocs from '../../../src/elements/button/docs'
+import checkboxDocs from '../../../src/elements/checkbox/docs'
+import formDocs from '../../../src/elements/form/docs'
+import imageDocs from '../../../src/elements/image/docs'
+import inputDocs from '../../../src/elements/input/docs'
+import radioDocs from '../../../src/elements/radio/docs'
+import selectDocs from '../../../src/elements/select/docs'
+import tableDocs from '../../../src/elements/table/docs'
 
 const mapper = {
+  avatar: avatarDocs,
+  badge: badgeDocs,
   box: boxDocs,
+  button: buttonDocs,
+  checkbox: checkboxDocs,
+  form: formDocs,
+  image: imageDocs,
+  input: inputDocs,
+  radio: radioDocs,
+  select: selectDocs,
+  table: tableDocs,
 }
 
 function getElement(name) {
