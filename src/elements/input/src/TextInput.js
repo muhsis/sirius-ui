5ed5dc82b3ca158f3681a1152f136429e68fb0c5
Emre Kılınç <emre@nitro.io>
2019-04-03 19:21:37 +0300

    feat: add basic text input element

---
+++ src/elements/input/src/TextInput.js
@@ -0,0 +1,83 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import styled, { css, withTheme } from 'styled-components'
+import { space } from 'styled-system'
+import { controlFocus, transitionBase } from '../../../theming'
+
+const sizeStyles = {
+  sm: p => css`
+    padding: ${p.theme.inputPaddingYSm} ${p.theme.inputPaddingXSm};
+    font-size: ${p.theme.fontSizeSm};
+    line-height: ${p.theme.inputLineHeightSm};
+    border-radius: ${p.theme.borderRadiusSm};
+  `,
+  md: p => css`
+    padding: ${p.theme.inputPaddingY} ${p.theme.inputPaddingX};
+    font-size: ${p.theme.fontSizeBase};
+    line-height: ${p.theme.inputLineHeight};
+    border-radius: ${p.theme.borderRadius};
+  `,
+  lg: p => css`
+    padding: ${p.theme.inputPaddingYLg} ${p.theme.inputPaddingXLg};
+    font-size: ${p.theme.fontSizeLg};
+    line-height: ${p.theme.inputLineHeightLg};
+    border-radius: ${p.theme.borderRadiusLg};
+  `,
+}
+
+function createStyledComponent(size, props) {
+  const sizeStyle = sizeStyles[size]
+
+  const ScInput = styled.input`
+    display: block;
+    width: 100%;
+    border-width: ${p => p.theme.inputBorderWidth};
+    border-color: ${p => p.theme.inputBorderColor};
+    border-style: solid;
+    line-height: ${p => p.theme.inputLineHeight};
+    color: ${p => p.theme.inputTextColor};
+    font-family: ${p => p.theme.fontFamily};
+
+    &[type='number'] {
+      padding-right: 6px;
+    }
+
+    &::placeholder {
+      color: ${p => p.theme.inputPlaceholderText};
+    }
+
+    &:focus {
+      ${p => controlFocus(p.theme.colors.primary)(p)}
+    }
+
+    &:disabled {
+      background-color: ${p => p.theme.inputDisabledBgColor};
+      color: ${p => p.theme.inputDisabledText};
+    }
+
+    ${p => transitionBase(p)};
+    ${p => sizeStyle(p)};
+
+    ${space}
+  `
+
+  return <ScInput {...props} />
+}
+
+class TextInput extends PureComponent {
+  static propTypes = {
+    size: PropTypes.oneOf(['sm', 'md', 'lg']),
+    ...space.propTypes,
+  }
+
+  static defaultProps = {
+    size: 'md',
+  }
+
+  render() {
+    return createStyledComponent(this.props.size, ...this.props)
+  }
+}
+TextInput.displayName = 'TextInput'
+
+export default withTheme(TextInput)
