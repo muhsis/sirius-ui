b56fb9de96af06e620808fabdc3d13fa5962c972
Emre Kılınç <emre@nitro.io>
2019-04-03 14:40:19 +0300

    feat: change default typography theme values

--- src/theming/typography.js
+++ src/theming/typography.js
@@ -2,16 +2,16 @@ import { modularScale } from 'polished'
 
 export const fontSizes = [12, 14, 16, 20, 24, 32, 48, 64, 72]
 export const fontFamilyBase =
-  '"SF UI Text", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+  '"Mukta", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
 export const fontFamilyDisplay =
-  '"SF UI Display", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+  '"Mukta", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
 export const fontFamilyMonospace =
   '"SF Mono", "Monaco", "Inconsolata", "Fira Mono", "Droid Sans Mono", "Source Code Pro", monospace'
 export const fontFamily = fontFamilyBase
 
-export const fontSizeBase = '1rem'
-export const fontSizeSm = modularScale(-1, fontSizeBase)
-export const fontSizeLg = modularScale(1, fontSizeBase)
+export const fontSizeBase = '.925rem'
+export const fontSizeSm = modularScale(-0.25, fontSizeBase)
+export const fontSizeLg = modularScale(1.5, fontSizeBase)
 
 export const fontWeightLight = 300
 export const fontWeightNormal = 400
