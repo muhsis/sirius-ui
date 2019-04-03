4081ad6b5eb6330a7a5a8c4722faaa37c2a344e9
Emre Kılınç <emre@nitro.io>
2019-04-03 14:41:14 +0300

    feat: remove global html,body styles from normalize

--- src/components/normalize/src/Normalize.js
+++ src/components/normalize/src/Normalize.js
@@ -1,16 +1,8 @@
 import { normalize } from 'polished'
 import { createGlobalStyle } from 'styled-components'
 
-import { fontFamily, fontSizeBase, lineHeightBase } from '../../../theming'
-
 export const Normalize = createGlobalStyle`
   ${normalize()}
-
-  html, body {
-    font-family: ${fontFamily};
-    font-size: ${fontSizeBase};
-    line-height: ${lineHeightBase};
-  }
 
   * {
     box-sizing: border-box;
