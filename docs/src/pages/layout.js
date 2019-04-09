6cd30c3628b2dae5dde87438e9a9f103ed21a5d3
Emre Kılınç <emre@nitro.io>
2019-04-09 16:30:39 +0300

    feat: examples for dropdown and sidesheet

--- docs/src/pages/layout.js
+++ docs/src/pages/layout.js
@@ -1,25 +1,103 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Layout, SEO, Sidebar } from '../components'
-import { Box, Text, Paragraph, boxiconsRegular } from '../../../src'
+import {
+  Box,
+  Text,
+  Paragraph,
+  Dropdown,
+  DropdownContent,
+  Button,
+  Menu,
+  MenuItem,
+  MenuDivider,
+  boxiconsRegular,
+  IconLogOut,
+  IconChevronDown,
+  IconAddress,
+  IconChat,
+  IconUser,
+  IconAircraft,
+} from '../../../src'
 
-const LayoutPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <Sidebar />
-    <Box p={3} ml={250}>
-      <Paragraph>
-        Maecenas faucibus mollis interdum. Curabitur blandit tempus porttitor.
-        Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur
-        blandit tempus porttitor. Vestibulum id ligula porta felis euismod
-        semper.
-      </Paragraph>
-      <Text color="primary" fontSize={1}>
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
-        lacinia odio sem nec elit. Maecenas sed diam eget risus varius blandit
-        sit amet non magna.
-      </Text>
-    </Box>
-  </Layout>
-)
+class LayoutPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleOpenCloseDropdown = this.handleOpenCloseDropdown.bind(this)
+    this.state = {
+      dropdownOpen: true,
+    }
+  }
+
+  handleOpenCloseDropdown() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <Sidebar />
+        <Box p={3} ml={250} mb={5}>
+          <Dropdown>
+            <Button onClick={this.handleOpenCloseDropdown}>
+              Dropdown <IconChevronDown />
+            </Button>
+            <DropdownContent minWidth={200} isOpen={this.state.dropdownOpen}>
+              <Menu>
+                <MenuItem>
+                  <IconAddress mr={2} />
+                  Sample Menu Item
+                </MenuItem>
+                <MenuItem onClick={this.handleOpenCloseDropdown}>
+                  <IconChat mr={2} />
+                  Sample Menu Item
+                </MenuItem>
+                <MenuItem>
+                  <IconUser mr={2} />
+                  Sample Menu Item
+                </MenuItem>
+                <MenuDivider />
+                <MenuItem>
+                  <IconAircraft mr={2} />
+                  Sample Menu Item
+                </MenuItem>
+                <MenuItem>
+                  <IconLogOut mr={2} /> Çıkış Yap
+                </MenuItem>
+              </Menu>
+            </DropdownContent>
+          </Dropdown>
+          <Paragraph>
+            Maecenas faucibus mollis interdum. Curabitur blandit tempus
+            porttitor. Morbi leo risus, porta ac consectetur ac, vestibulum at
+            eros. Curabitur blandit tempus porttitor. Vestibulum id ligula porta
+            felis euismod semper.
+          </Paragraph>
+          <Text color="primary" fontSize={1}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula,
+            eget lacinia odio sem nec elit. Maecenas sed diam eget risus varius
+            blandit sit amet non magna.
+          </Text>
+        </Box>
+
+        <Box p={3} ml={250} textAlign="right">
+          <Dropdown>
+            <Button onClick={this.handleOpenCloseDropdown}>Dropdown</Button>
+            <DropdownContent
+              align="right"
+              minWidth={200}
+              isOpen={this.state.dropdownOpen}
+            >
+              Dropdown content will go here...
+            </DropdownContent>
+          </Dropdown>
+        </Box>
+      </Layout>
+    )
+  }
+}
 
 export default LayoutPage
--- docs/src/pages/system.js
+++ docs/src/pages/system.js
@@ -1,25 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Layout, SEO, Sidebar } from '../components'
-import { Box, Text, Paragraph, boxiconsRegular } from '../../../src'
+import { Box, Text, Paragraph, SideSheet, Button } from '../../../src'
 
-const SystemPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <Sidebar />
-    <Box p={3} ml={250}>
-      <Paragraph>
-        Maecenas faucibus mollis interdum. Curabitur blandit tempus porttitor.
-        Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur
-        blandit tempus porttitor. Vestibulum id ligula porta felis euismod
-        semper.
-      </Paragraph>
-      <Text color="primary" fontSize={1}>
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
-        lacinia odio sem nec elit. Maecenas sed diam eget risus varius blandit
-        sit amet non magna.
-      </Text>
-    </Box>
-  </Layout>
-)
+class SystemPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.openSideSheet = this.openSideSheet.bind(this)
+    this.closeSideSheet = this.closeSideSheet.bind(this)
+    this.state = {
+      isShown: false,
+    }
+  }
+
+  openSideSheet() {
+    console.log('Open side sheet!')
+    this.setState({
+      isShown: true,
+    })
+  }
+
+  closeSideSheet() {
+    this.setState({
+      isShown: false,
+    })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <Sidebar />
+        <Box p={3} ml={250}>
+          <Button onClick={this.openSideSheet}>Show Side Sheet</Button>
+          <SideSheet
+            isShown={this.state.isShown}
+            onCloseComplete={this.closeSideSheet}
+          >
+            <Paragraph m={3}>Basic Example</Paragraph>
+          </SideSheet>
+        </Box>
+        <Box p={3} ml={250}>
+          <Paragraph>
+            Maecenas faucibus mollis interdum. Curabitur blandit tempus
+            porttitor. Morbi leo risus, porta ac consectetur ac, vestibulum at
+            eros. Curabitur blandit tempus porttitor. Vestibulum id ligula porta
+            felis euismod semper.
+          </Paragraph>
+          <Text color="primary" fontSize={1}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula,
+            eget lacinia odio sem nec elit. Maecenas sed diam eget risus varius
+            blandit sit amet non magna.
+          </Text>
+        </Box>
+      </Layout>
+    )
+  }
+}
 
 export default SystemPage
