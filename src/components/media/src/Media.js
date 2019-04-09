30f2fed7ab66371b4b34cb6aedd757925fcbe026
Emre Kılınç <emre@nitro.io>
2019-04-10 01:19:11 +0300

    feat: fix flex aligment for media items

--- src/components/media/src/Media.js
+++ src/components/media/src/Media.js
@@ -2,6 +2,8 @@ import styled, { withTheme } from 'styled-components'
 
 const Media = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: flex-start;
 `
 Media.displayName = 'Media'
 
