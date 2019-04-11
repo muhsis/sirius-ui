755f758b6bf7711d0c9e11105adc1467a2fda634
Emre Kılınç <emre@nitro.io>
2019-04-12 00:09:07 +0300

    feat: update badge docs

--- src/elements/badge/docs/index.js
+++ src/elements/badge/docs/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Badge from '../src/Badge'
-import Pill from '../src/Pill'
+import { Box } from '../../box'
 import { Paragraph, Code } from '../../typography'
 
 // Code examples
 import exampleBadge from './examples/badge.example'
-import examplePill from './examples/pill.example'
+import exampleDots from './examples/dots.example'
 
-const name = 'Badge & Pill'
+const name = 'Badge'
 const introduction = (
   <Paragraph>
     Documentation and examples for badges, our small count and labeling
@@ -15,7 +15,7 @@ const introduction = (
   </Paragraph>
 )
 
-const scope = { Badge, Pill }
+const scope = { Badge, Box }
 
 const components = [
   {
@@ -26,28 +26,25 @@ const components = [
         title: 'Badge Example',
         description: (
           <Paragraph>
-            Very basic <Code>Badge</Code> component examples.
+            Very basic <Code>Badge</Code> component examples. Note that if{' '}
+            <Code>count</Code> property is not set, or set to 0, badge will be
+            hidden.
           </Paragraph>
         ),
         codeText: exampleBadge,
         scope,
       },
-    ],
-  },
-  {
-    name: 'Pill',
-    underDevelopment: true,
-    examples: [
       {
-        title: 'Pill Example',
+        title: 'Dots Example',
         description: (
           <Paragraph>
-            Very basic <Code>Pill</Code> component examples. This is actually
-            not a pill component. Pill is something else entirely. Need to
-            change the name on this one!
+            Badges can be used as dot indicators. These indicators have a nice,
+            subtle animation. When <Code>isDot</Code> set to true, count will
+            not be shown. You can also use <Code>Text</Code> property of this
+            component when <Code>isDot</Code> mode set to true.
           </Paragraph>
         ),
-        codeText: examplePill,
+        codeText: exampleDots,
         scope,
       },
     ],
