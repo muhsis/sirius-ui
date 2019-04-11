2c784abdfa66113ffa7ebf1562117a210b3765e9
Emre Kılınç <emre@nitro.io>
2019-04-12 02:10:08 +0300

    docs: complete button docs

--- src/elements/table/src/TableCell.js
+++ src/elements/table/src/TableCell.js
@@ -3,6 +3,7 @@ import { color, textAlign } from 'styled-system'
 
 const TableCell = styled.td`
   border: inherit;
+  font-size: ${p => p.theme.fontSizeBase};
   padding: ${p => p.theme.tableCellPadding};
   vertical-align: middle;
 
--- src/elements/table/src/TableHeaderCell.js
+++ src/elements/table/src/TableHeaderCell.js
@@ -4,6 +4,7 @@ import { color, textAlign } from 'styled-system'
 const TableHeaderCell = styled.th`
   background-color: ${p => p.theme.grays.snow};
   border: inherit;
+  font-size: ${p => p.theme.fontSizeBase};
   font-weight: ${p => p.theme.tableHeaderCellFontWeight};
   padding: ${p => p.theme.tableCellPadding};
   text-align: left;
