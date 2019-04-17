57efb4e457ea177678858d4d208b14a179b98c2f
Emre Kılınç <emre@nitro.io>
2019-04-17 19:31:10 +0300

    fix: add TextInput height

--- src/elements/input/src/TextInput.js
+++ src/elements/input/src/TextInput.js
@@ -10,30 +10,48 @@ const sizeStyles = {
     font-size: ${p.theme.fontSizeSm};
     line-height: ${p.theme.inputLineHeightSm};
     border-radius: ${p.theme.borderRadiusSm};
+    height: calc(
+      ${p.theme.inputLineHeightSm}em + ${p.theme.inputTotalPaddingYSm} + 2px
+    );
   `,
   md: p => css`
     padding: ${p.theme.inputPaddingY} ${p.theme.inputPaddingX};
     font-size: ${p.theme.fontSizeBase};
     line-height: ${p.theme.inputLineHeight};
     border-radius: ${p.theme.borderRadius};
+    height: calc(
+      ${p.theme.inputLineHeight}em + ${p.theme.inputTotalPaddingY} + 2px
+    );
   `,
   lg: p => css`
     padding: ${p.theme.inputPaddingYLg} ${p.theme.inputPaddingXLg};
     font-size: ${p.theme.fontSizeLg};
     line-height: ${p.theme.inputLineHeightLg};
     border-radius: ${p.theme.borderRadiusLg};
+    height: calc(
+      ${p.theme.inputLineHeightLg}em + ${p.theme.inputTotalPaddingYLg} + 2px
+    );
   `,
 }
 
 const ScInput = styled.input`
-  display: block;
-  width: 100%;
-  border-width: ${p => p.theme.inputBorderWidth};
-  border-color: ${p => p.theme.inputBorderColor};
-  border-style: solid;
-  line-height: ${p => p.theme.inputLineHeight};
   color: ${p => p.theme.inputTextColor};
   font-family: ${p => p.theme.fontFamily};
+  display: block;
+  width: 100%;
+
+  line-height: ${p => p.theme.inputLineHeight};
+  background-color: white;
+  background-clip: padding-box;
+  border: ${p => p.theme.inputBorderWidth} solid
+    ${p => p.theme.inputBorderColor};
+
+  ${p =>
+    p.inline &&
+    css`
+      display: inline-block;
+      width: auto;
+    `}
 
   &[type='number'] {
     padding-right: 6px;
@@ -41,6 +59,8 @@ const ScInput = styled.input`
 
   &::placeholder {
     color: ${p => p.theme.inputPlaceholderText};
+    /* Override Firefox's unusual default opacity; see https://github.com/twbs/bootstrap/pull/11526. */
+    opacity: 1;
   }
 
   &:focus {
@@ -58,22 +78,34 @@ const ScInput = styled.input`
   ${space}
 `
 
-function createStyledComponent(size, props) {
-  return <ScInput size={size} {...props} />
+function createStyledComponent(size, inline, props) {
+  return <ScInput size={size} inline={inline} {...props} />
 }
 
 class TextInput extends PureComponent {
   static propTypes = {
+    /**
+     * Size of input. Same as button. Default is 'md'
+     */
     size: PropTypes.oneOf(['sm', 'md', 'lg']),
+    /**
+     * Pass this prop if the input is display: inline-block and width: auto.
+     */
+    inline: PropTypes.bool,
     ...space.propTypes,
   }
 
   static defaultProps = {
     size: 'md',
+    inline: false,
   }
 
   render() {
-    return createStyledComponent(this.props.size, ...this.props)
+    return createStyledComponent(
+      this.props.size,
+      this.props.inline,
+      ...this.props,
+    )
   }
 }
 TextInput.displayName = 'TextInput'
