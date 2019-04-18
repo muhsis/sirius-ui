20b9b565fa3eb80449a6ec5f0cf0320e426fb643
Emre Kılınç <emre@nitro.io>
2019-04-18 16:44:05 +0300

    docs: complete skeleton docs

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -17,22 +17,6 @@ console.log('DT', DataTable)
 
 const toaster = new Toaster(theme)
 
-const columns = ['Product', 'Price', 'SKU Number', 'Net quantity', 'Net sales']
-
-const rows = [
-  ['Emerald Silk Gown', '$875.00', 124689, 140, '$122,500.00'],
-  ['Mauve Cashmere Scarf', '$230.00', 124533, 83, '$19,090.00'],
-  [
-    'Navy Merino Wool Blazer with khaki chinos and yellow belt',
-    '$445.00',
-    124518,
-    32,
-    '$14,240.00',
-  ],
-]
-
-const totals = ['Toplam : ', '', '', 255, '$155,830.00']
-
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
@@ -45,61 +29,6 @@ const IndexPage = () => (
           developed with <a href="#">Styled Components 💅</a> and{' '}
           <a href="#">Styled System</a>.
         </Paragraph>
-
-        <Box my={5}>
-          <TextInput type="text" name="test" inline mr={0} />
-          <Button mx={1}>Sample</Button>
-
-          <NumberInput
-            thousandSeparator={true}
-            suffix={' ₺'}
-            placeholder="Enter Money Value"
-            mr={1}
-            inline
-          />
-          <NumberInput
-            thousandSeparator={true}
-            suffix={' ₺'}
-            placeholder="2,450"
-            width={100}
-            mr={1}
-            inline
-          />
-        </Box>
-
-        <Box my={5}>
-          <TextInput type="text" name="test" mr={0} size="sm" inline />
-          <Button mr={4} size="sm">
-            Sample
-          </Button>
-        </Box>
-
-        <Box my={5}>
-          <TextInput type="text" name="test" mr={0} size="lg" inline />
-          <Button mr={4} size="lg">
-            Sample
-          </Button>
-        </Box>
-
-        <Box my={5}>
-          <Box display="inline-block" mr={2}>
-            <TextInput type="text" size="sm" />
-          </Box>
-          <Box display="inline-block" mr={2}>
-            <TextInput type="text" />
-          </Box>
-          <Box display="inline-block" mr={2}>
-            <TextInput type="text" size="lg" />
-          </Box>
-        </Box>
-
-        <Box my={5}>
-          <Button size="sm">Button</Button>
-          <Button>Button</Button>
-          <Button size="lg">Button</Button>
-        </Box>
-
-        <DataTable columns={columns} rows={rows} totals={totals} />
       </Content>
     </Container>
   </Layout>
--- src/components/skeleton/docs/index.js
+++ src/components/skeleton/docs/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { SkeletonBodyText } from '..'
+import { SkeletonBodyText, SkeletonHeading, SkeletonThumbnail } from '..'
 import { Paragraph, Code, Box } from '../../..'
 
 /* eslint-disable import/no-unresolved, import/no-webpack-loader-syntax */
 import sourceSkeletonBodyText from '!raw-loader!../src/SkeletonBodyText'
+import sourceSkeletonHeading from '!raw-loader!../src/SkeletonHeading'
+import sourceSkeletonThumbnail from '!raw-loader!../src/SkeletonThumbnail'
 /* eslint-enable import/no-unresolved, import/no-webpack-loader-syntax */
 
 // Code examples
 import exampleBody from './examples/body.example'
+import exampleHeading from './examples/heading.example'
+import exampleHeadingVariants from './examples/heading-variants.example'
+import exampleThumbnail from './examples/thumbnail.example'
 
 const name = 'Skeleton'
 const introduction = (
@@ -18,13 +23,13 @@ const introduction = (
   </Paragraph>
 )
 
-const scope = { Box, SkeletonBodyText }
+const scope = { Box, SkeletonBodyText, SkeletonHeading, SkeletonThumbnail }
 
 const components = [
   {
     name: 'Skeleton Body Text',
     source: sourceSkeletonBodyText,
-    underDevelopment: true,
+    underDevelopment: false,
     examples: [
       {
         title: 'Body Text Examples',
@@ -35,6 +40,55 @@ const components = [
           </Paragraph>
         ),
         codeText: exampleBody,
+        scope,
+      },
+    ],
+  },
+  {
+    name: 'Skeleton Heading',
+    source: sourceSkeletonHeading,
+    underDevelopment: false,
+    examples: [
+      {
+        title: 'Heading Examples',
+        description: (
+          <Paragraph>
+            <Code>SkeletonHeading</Code> component also takes <Code>width</Code>{' '}
+            to render heading with desired width. Default width is{' '}
+            <Code>200px</Code>.
+          </Paragraph>
+        ),
+        codeText: exampleHeading,
+        scope,
+      },
+      {
+        title: 'Heading Variants',
+        description: (
+          <Paragraph>
+            <Code>SkeletonHeading</Code> can also take <Code>variant</Code>{' '}
+            property to match the exact same height of headings.
+          </Paragraph>
+        ),
+        codeText: exampleHeadingVariants,
+        scope,
+      },
+    ],
+  },
+  {
+    name: 'Skeleton Thumbnail',
+    source: sourceSkeletonThumbnail,
+    underDevelopment: false,
+    examples: [
+      {
+        title: 'Thumbnail Examples',
+        description: (
+          <Paragraph>
+            <Code>SkeletonThumbnail</Code> component takes <Code>width</Code>{' '}
+            and <Code>height</Code> properties. And also{' '}
+            <Code>borderRadius</Code>.
+          </Paragraph>
+        ),
+        codeText: exampleThumbnail,
         scope,
       },
     ],
