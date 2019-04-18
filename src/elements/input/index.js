f5c820797f148d9c6ec295cb0cf391ff9e45a30b
Emre Kılınç <emre@nitro.io>
2019-04-18 16:55:09 +0300

    feat: add SelectInput component

--- src/elements/input/index.js
+++ src/elements/input/index.js
@@ -1,3 +1,4 @@
-export { default as TextInput } from './src/TextInput'
 export { default as NumberInput } from './src/NumberInput'
+export { default as SelectInput } from './src/SelectInput'
+export { default as TextInput } from './src/TextInput'
 export { default as TextArea } from './src/TextArea'
