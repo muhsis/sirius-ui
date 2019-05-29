8dabaebab1367c89587c8dfe6e08d1f7263835f3
Emre Kılınç <emre@nitro.io>
2019-05-29 22:55:57 +0300

    feat: make MenuItem get variant prop

--- src/components/menu/src/MenuItem.js
+++ src/components/menu/src/MenuItem.js
@@ -1,7 +1,9 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import styled, { withTheme } from 'styled-components'
 import { colorYik } from '../../../theming/color'
 
-const MenuItem = styled.button`
+const ScMenuItem = styled.button`
   border: none;
   border-radius: ${p => p.theme.borderRadius};
   font-size: ${p => p.theme.fontSizeBase};
@@ -16,21 +18,49 @@ const MenuItem = styled.button`
   transition: color 0.15s, background-color 0.15s;
 
   & svg {
-    color: ${p => p.theme.colors.primary};
+    color: ${p => p.theme.colors[p.variant]};
   }
 
   &:hover {
-    background-color: ${p => p.theme.tints.primary};
+    background-color: ${p => p.theme.tints[p.variant]};
   }
 
   &:active {
-    background-color: ${p => p.theme.colors.primary};
-    color: ${p => colorYik(p.theme.colors.primary)(p)};
+    background-color: ${p => p.theme.colors[p.variant]};
+    color: ${p => colorYik(p.theme.colors[p.variant])(p)};
   }
 
   &:active svg {
-    color: ${p => colorYik(p.theme.colors.primary)(p)};
+    color: ${p => colorYik(p.theme.colors[p.variant])(p)};
   }
 `
+
+class MenuItem extends PureComponent {
+  static propTypes = {
+    /**
+     * Contextual color option.
+     */
+    variant: PropTypes.oneOf([
+      'primary',
+      'secondary',
+      'success',
+      'info',
+      'warning',
+      'danger',
+      'light',
+      'dark',
+    ]),
+  }
+
+  static defaultProps = {
+    variant: 'primary',
+  }
+
+  render() {
+    const { variant, children } = this.props
+
+    return <ScMenuItem variant={variant}>{children}</ScMenuItem>
+  }
+}
 
 export default withTheme(MenuItem)
